cb1ea8ab663a533e0a5bcaf639f609ee0b493597
BodoBurger <bodo.burger@gmail.com>
2018-05-25 22:21:07 +0200

    init describe and prepare

---
+++ R/describe.R
@@ -0,0 +1,18 @@
+#' Description of a data object
+#'
+#' Description of a data object like a data.frame that is more comprehensive than basic R functions
+#' like str() or summary()
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+describe = function(x) {
+
+}
+
+
+
+
