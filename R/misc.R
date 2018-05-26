d1000bdb0c5f72e0d6d49883e0c85ca3a3366147
BodoBurger <bodo.burger@gmail.com>
2018-05-26 13:46:09 +0200

    help index function

--- R/misc.R
+++ R/misc.R
@@ -13,3 +13,18 @@ factorMapping = function(x) {
   if(!is.factor(x)) stop("Object is not a factor.")
   data.frame(levels = unique(x), value = as.numeric(unique(x)))
 }
+
+#' Show package documentation index (help index)
+#'
+#' This is just a shorter way of calling \code{help(package = "package name")}.
+#' If you use RStudio you do not have to type the quotation marks (""),
+#' just autocomplete the package name by pressing TAB.
+#'
+#' @param package name of the package
+#'
+#' @export
+#'
+#' @examples
+hi = function(package) {
+  eval(call("help", package = package))
+}
