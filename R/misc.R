f3630e3453af687767b11d311b46d916e0aeb46e
BodoBurger <bodo.burger@gmail.com>
2018-06-09 11:44:21 +0200

    Improved print.data.frame

--- R/misc.R
+++ R/misc.R
@@ -54,3 +54,33 @@ FactorMapping = function(x) {
 hi = function(package) {
   eval(call("help", package = package))
 }
+
+
+#' Improved print.data.frame
+#'
+#' If the number of rows of a data.frame exceeds \code{max.rows}
+#' only head and tail of the data.frame is printed.
+#'
+#' @param data data.frame
+#' @param all.rows=FALSE If TRUE, print out all rows.
+#' @param max.rows=15 integer Threshold up to all rows are printed out.
+#'
+#' @export
+#'
+#' @examples
+#' print(mtcars)
+#'
+#' print(mtcars, all.rows=TRUE)
+print.data.frame = function(data, all.rows=FALSE, max.rows=15) {
+  if (nrow(data) <= max.rows | all.rows) {
+    base::print.data.frame(data)
+  } else {
+    capt.print = capture.output(base::print.data.frame(data))
+    end = length(capt.print)
+    ellipsis = paste(rep("...   ", nchar(capt.print[1])/6), collapse = "")
+    # TODO: make ellipsis line look nicer ;)
+    cat(capt.print[1:6], sep = "\n")
+    cat(ellipsis, "\n")
+    cat(capt.print[(end-4):end], sep = "\n")
+  }
+}
