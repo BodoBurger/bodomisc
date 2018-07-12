546f184e1565c0119f15414a9da1f7437f3c3163
BodoBurger <bodo.burger@gmail.com>
2018-07-12 11:31:15 +0200

    Bin vector based on quantiles

--- R/prepare.R
+++ R/prepare.R
@@ -3,9 +3,40 @@
 #' @param x
 #'
 #' @return
-#' @export
 #'
 #' @examples
 prepare = function(x) {
 
+}
+
+#' Put values of vector in bins based on quantiles.
+#'
+#' @param x (numeric) Vector you want to bin.
+#' @param probs (numeric) Default is seq(0, 1, .25) (i.e. quartiles).
+#' @param include.lowest=TRUE (logical) Include element with lowest value. Who would not want that
+#'   (except the authors of the base cut function)?
+#' @param as.factor=FALSE (logical) If TRUE returns a factor with quantile boundaries as level
+#'   labels. Level labels can be changed via the labels argument.
+#' @param values.as.names=FALSE (logical) Return named vector with values of the input
+#'   so that you can see how bin numbers correspond to values of x.
+#' @param ... further arguments passed down to the base functions quantile() and cut()
+#'
+#' @return integer(length(x)) or factor
+#' @export
+#'
+#' @examples
+#' n = 200
+#' x = runif(n, min = 0, max = 1)
+#' x1 = x + rnorm(n, 0, 0.05)
+#' x2 = x + rnorm(n, 0, 0.05)
+#' y = x1 + x2^2 + rnorm(n, 0, 0.1)
+#' plot(x1, x2, cex = quantileBins(y, probs = seq(0,1,.1))/5,
+#'   col = quantileBins(y))
+quantileBins = function(x, probs = c(0, .25, .5, .75, 1), include.lowest = TRUE, as.factor = FALSE,
+    values.as.names = FALSE, ...) {
+  bins = cut(x, quantile(x, probs = probs, ...), include.lowest = include.lowest, ...)
+  if (as.factor) return(bins)
+  else {
+    if (values.as.names) return(setNames(as.integer(bins), x)) else return(as.integer(bins))
+  }
 }
