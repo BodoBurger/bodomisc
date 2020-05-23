e97b15944f9db36ac1b8b98e3881334eaaffd6ec
BodoBurger <bodo.burger@gmail.com>
2020-05-23 20:32:32 +0200

    update

---
+++ R/install_essential_packages.R
@@ -0,0 +1,8 @@
+
+#' https://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them
+install_essential_packages = function() {
+# "tidyverse"
+# install.packages(c("mlr", "mlrMBO", "caret", "e1071", "ElemStatLearn", "gam", "gbm", "xgboost", "gridExtra", "ICEbox", "mboost", "mvtnorm", "partykit", "randomForest", "ranger", "ALEPlot", "AER", "ada", "adabag", "mlbench", "penalized"))
+# install.packages(c("Hmisc", "margins", "directlabels", "ggforce", "leaps"))
+
+}
